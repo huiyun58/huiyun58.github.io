5d7a15b196a0ac9da7332e0686409c1ae6024b07
huiyun58 <b03502015@ntu.edu.tw>
2017-07-22 07:37:29 +0800

    1

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,19 +1,33 @@
 // 請 jQuery 抓 button 元件，並且設定當 click 事件發生後，要執行的程式碼
-var m = $('#mass').val()
-var r = $('#radius').val()
-var h = $('#height').val()
-m = Number(m)
-h = Number(h)
-r = Number(r)
-Ixy = 1 / 12 * m * (3 * r * r + h * h)
-Iz = 1 / 2 * m * r * r
+
 
 $('button1').on('click', function() {
+    var m = $('#mass').val()
+    var r = $('#radius').val()
+    var h = $('#height').val()
+    m = Number(m)
+    h = Number(h)
+    r = Number(r)
+    Ixy = 1 / 12 * m * (3 * r * r + h * h)
     $('#result').val(Ixy)
 })
 $('button2').on('click', function() {
+    var m = $('#mass').val()
+    var r = $('#radius').val()
+    var h = $('#height').val()
+    m = Number(m)
+    h = Number(h)
+    r = Number(r)
+    Ixy = 1 / 12 * m * (3 * r * r + h * h)
     $('#result').val(Ixy)
 })
 $('button3').on('click', function() {
+    var m = $('#mass').val()
+    var r = $('#radius').val()
+    var h = $('#height').val()
+    m = Number(m)
+    h = Number(h)
+    r = Number(r)
+    Iz = 1 / 2 * m * r * r
     $('#result').val(Iz)
 })
