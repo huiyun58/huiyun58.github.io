106d54ee10d62b6f65607b27d4a82859e2d8babb
huiyun58 <b03502015@ntu.edu.tw>
2017-08-01 14:55:54 +0800

    1

--- hw1_2/js/creative.js
+++ hw1_2/js/creative.js
@@ -1,7 +1,80 @@
 (function($) {
     "use strict"; // Start of use strict
+    $('button1').on('click', function() {
+        var m = $('#mass').val()
+        var r = $('#radius').val()
+        var h = $('#height').val()
+        m = Number(m)
+        h = Number(h)
+        r = Number(r)
+        Ixy = 1 / 12 * m * (3 * r * r + h * h)
+        $('#result1').val(Ixy)
+    })
+    $('button2').on('click', function() {
+        var m = $('#mass').val()
+        var r = $('#radius').val()
+        var h = $('#height').val()
+        m = Number(m)
+        h = Number(h)
+        r = Number(r)
+        Ixy = 1 / 12 * m * (3 * r * r + h * h)
+        $('#result1').val(Ixy)
+    })
+    $('button3').on('click', function() {
+        var m = $('#mass').val()
+        var r = $('#radius').val()
+        var h = $('#height').val()
+        m = Number(m)
+        h = Number(h)
+        r = Number(r)
+        Iz = 1 / 2 * m * r * r
+        $('#result1').val(Iz)
+    })
+    $('button4').on('click', function() {
+        var m = $('#mass').val()
+        var r = $('#radius').val()
+        var h = $('#height').val()
+        m = Number(m)
+        h = Number(h)
+        r = Number(r)
+        Ishell = m * r * r
+        $('#result1').val(Ishell)
+    })
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('button5').on('click', function() {
+        var m = $('#spheremass').val()
+        var r = $('#sphereradius').val()
+        m = Number(m)
+        r = Number(r)
+        Isolid = 2 / 5 * m * r * r
+        $('#result2').val(Isolid)
+    })
+    $('button6').on('click', function() {
+        var m = $('#spheremass').val()
+        var r = $('#sphereradius').val()
+        m = Number(m)
+        r = Number(r)
+        Ishallow = 2 / 3 * m * r * r
+        $('#result2').val(Ishallow)
+    })
+
+    $('button7').on('click', function() {
+        var h = $('#plateheight').val()
+        var w = $('#platewidth').val()
+        h = Number(h)
+        w = Number(w)
+        Imiddle = 1 / 12 * m * (h * h + w * w)
+        $('#result3').val(Imiddle)
+    })
+    $('button8').on('click', function() {
+            var h = $('#plateheight').val()
+            var w = $('#platewidth').val()
+            h = Number(h)
+            w = Number(w)
+            Iend = 1 / 12 m * (4 * h * h + w * w)
+            $('#result3').val(Iend)
+        })
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
