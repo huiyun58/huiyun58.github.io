3ad236045de071ca4b7ae82c91a29bf5a3ac6318
huiyun58 <b03502015@ntu.edu.tw>
2017-08-07 18:32:36 +0800

    1

---
+++ hw2/js/index.js
@@ -0,0 +1,44 @@
+$('#insert').on('click', function() {
+    var data = {
+            name: $('#InputProductName').val(),
+            // +直接取數值非字串
+            price: +$('#InputProductPrice').val(),
+            count: +$('#InputProductCount').val(),
+            image: $('#InputProductImage').val()
+        }
+        // "http://js2017-hw2.kchen.club/insert"
+    $.post("http://js2017-hw2.kchen.club/insert", data, function(response) {
+        if (response) {
+            if (response.result) {
+                $('#message').text('新增成功');
+            } else {
+                $('#message').text('新增失敗 (' + response.data + ')');
+            }
+        } else {
+            $('#message').text('新增失敗');
+        }
+        $('#dialog').modal('show');
+        console.log(response);
+    }, "json");
+})
+
+$('#query').on('click', function() {
+    $.get("http://js2017-hw2.kchen.club/query", function(response) {
+        if (response) {
+            if (response.result) {
+
+                console.log(response.data[0].name);
+
+
+
+            } else {
+                $('#message').text('查詢失敗 (' + response.data + ')');
+                $('#dialog').modal('show');
+            }
+        } else {
+            $('#message').text('查詢失敗');
+            $('#dialog').modal('show');
+        }
+        console.log(response);
+    }, "json");
+})
