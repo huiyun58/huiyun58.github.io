f0fe3d5066aee166d231c0b9bf632908ca0318b2
huiyun58 <b03502015@ntu.edu.tw>
2017-08-08 04:35:49 +0800

    1

--- hw2/js/index.js
+++ hw2/js/index.js
@@ -23,41 +23,100 @@ $('#insert').on('click', function() {
 })
 
 $('#query').on('click', function() {
-    $.get("http://js2017-hw2.kchen.club/query", function(response) {
-        if (response) {
-            if (response.result) {
+    var page = 1;
+    console.log(page);
 
-                for (var i = 0; i < response.data.length - 5; i++) {
-                    var data2 = [
-                        response.data[i]._id,
-                        response.data[i].name,
-                        response.data[i].price,
-                        response.data[i].count,
-                        response.data[i].image
-                    ];
+    var fun = function(page) {
+        $.get("http://js2017-hw2.kchen.club/query", function(response) {
+            if (response) {
+                if (response.result) {
 
+                    for (var i = 32 * (page - 1); i < 32 * (page); i++) {
+                        if (i < response.data.length - 5) {
+                            var data2 = [
+                                response.data[i]._id,
+                                response.data[i].name,
+                                response.data[i].price,
+                                response.data[i].count,
+                                response.data[i].image
+                            ];
 
+                            $div = $('<div>').attr('class', 'item').append('<img class="thumb" src="' + data2[4] + '">' + '<h3 class="title">' + data2[1] + '</h3>' + '<p class="price">NT$' + data2[2] + '</p>');
+                            $col = $('<div>').attr('class', 'col-*').append($div);
+                            $('#product-list').attr('class', 'row justify-content-around').append($col);
+                        }
+                        // alert("ID: " + data2[0] + "\nName: " + data2[1] + "\nPrice: " + data2[2] + "\nCount: " + data2[3] + "\nImg:" + data2[4]);
+                    }
 
-
-                    $div = $('<div>').attr('class', 'item').append('<img class="thumb" src="' + data2[4] + '">' + '<h3 class="title">' + data2[1] + '</h3>' + '<p class="price">NT$' + data2[2] + '</p>');
-                    $col = $('<div>').attr('class', 'col-*').append($div);
-                    $('#product-list').attr('class', 'row justify-content-around').append($col);
-                    // alert("ID: " + data2[0] + "\nName: " + data2[1] + "\nPrice: " + data2[2] + "\nCount: " + data2[3] + "\nImg:" + data2[4]);
+                } else {
+                    $('#message').text('查詢失敗 (' + response.data + ')');
+                    $('#dialog').modal('show');
                 }
 
 
-                console.log(response.data[0].name);
-
-
-
             } else {
-                $('#message').text('查詢失敗 (' + response.data + ')');
+                $('#message').text('查詢失敗');
                 $('#dialog').modal('show');
             }
-        } else {
-            $('#message').text('查詢失敗');
-            $('#dialog').modal('show');
+            // console.log(response);
+        }, "json")
+    }
+
+    fun(page);
+
+
+
+    $('#a1').on('click', function() {
+        page = 1;
+        console.log(page);
+        $("#product-list").empty();
+        fun(1);
+    })
+    $('#a2').on('click', function() {
+        page = 2;
+        console.log(page);
+        $("#product-list").empty();
+        fun(2);
+    })
+    $('#a3').on('click', function() {
+        page = 3;
+        console.log(page);
+        $("#product-list").empty();
+        fun(3);
+    })
+    $('#a4').on('click', function() {
+        page = 4;
+        console.log(page);
+        $("#product-list").empty();
+        fun(4);
+    })
+    $('#a5').on('click', function() {
+        page = 5;
+        console.log(page);
+        $("#product-list").empty();
+        fun(5);
+    })
+
+
+    $('#anxt').on('click', function() {
+        if (page < 5) {
+            page++;
         }
-        console.log(response);
-    }, "json");
+        console.log(page);
+        $("#product-list").empty();
+        fun(page);
+    })
+    $('#apre').on('click', function() {
+        if (page > 6) {
+            page--;
+        }
+        console.log(page);
+        $("#product-list").empty();
+        fun(page);
+    })
+
+
+
+
+
 })
